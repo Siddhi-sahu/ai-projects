d24256c83b2505b0f35a43754ccba37601637553
Siddhi Sahu <himanisahu739@gmail.com>
2025-04-10 19:13:46 +0530

    add typescript setup

--- fundamentals/src/index.ts
+++ fundamentals/src/index.ts
@@ -1,12 +1,20 @@
 import { OpenAI } from "openai";
+import dotenv from 'dotenv';
+dotenv.config();
 
+
+if (!process.env.OPENAI_API_KEY) {
+    throw new Error("OPENAI_API_KEY is not set");
+}
 //create an insatance of OpenAi class
 
-const openai = new OpenAI(process.env.OPENAI_API_KEY);
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+});
 
 const main = async () => {
     //define the prompt
-    const prompt = "I need to start resistance training. is it good for me? Limit it in 20 words or less.";
+    const prompt = "I need to start resistance training. is it good for me? Limit it in 10 words or less.";
 
     //send the api request
     const response = await openai.chat.completions.create({
