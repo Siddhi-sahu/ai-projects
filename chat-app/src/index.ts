c68fe20ac54d664eb46d93d1466168d8cdd2fcfa
Siddhi Sahu <himanisahu739@gmail.com>
2025-04-17 15:45:51 +0530

    add debugger setup for limiting context

--- chat-app/src/index.ts
+++ chat-app/src/index.ts
@@ -5,24 +5,38 @@ import { OpenAI } from "openai";
 //create a new open ai client
 const openai = new OpenAI();
 
+// 1st => define context with system instructions
+const context: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+    {
+        role: "system",
+        content: "Act like a girl's girl. Limit your responses in 10 words or less."
 
+    }
+]
+
+const createChat = async () => {
+    const response = await openai.chat.completions.create({
+        model: 'gpt-4o-mini',
+        messages: context
+    });
+
+    //3rd => everytime there is ai response we push that too to the context
+    const responseMessage = response.choices[0].message;
+    context.push(responseMessage);
+
+    console.log(responseMessage.content);
+
+}
+
+// process.stdin.addListener is used to read user input from the command line
 process.stdin.addListener("data", async (input) => {
     const userInput = input.toString().trim();
 
-    const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-            {
-                role: "system",
-                content: "Act like a girl's girl"
-            },
-            {
-                role: "user",
-                content: userInput
-
-            }
-        ]
+    //2nd => push the user prompt to the context
+    context.push({
+        role: "user",
+        content: userInput
     });
 
-    console.log(response.choices[0].message.content)
+    await createChat();
 })
--- fundamentals/src/index.ts
+++ fundamentals/src/index.ts
@@ -1,5 +1,6 @@
 import { OpenAI } from "openai";
 import dotenv from 'dotenv';
+import { encoding_for_model } from "tiktoken";
 dotenv.config();
 
 
@@ -14,7 +15,7 @@ const openai = new OpenAI({
 
 const main = async () => {
     //define the prompt
-    const prompt = "I need to start resistance training. is it good for me? Limit it in 10 words or less.";
+    const prompt = "I need to start resistance training. is it good for me?limit in 10 words or less";
 
     //send the api request
     const response = await openai.chat.completions.create({
@@ -24,14 +25,32 @@ const main = async () => {
                 role: "user",
                 content: prompt
             }
-        ]
+        ],
+        // max_tokens: 30,
+        //more choices, n = number of outputs the model generate
+        // n: 2
     });
 
     //Print the response
     console.log(response.choices[0].message.content);
+    //if n=2; message = whole message
+    // console.log(response.choices[1].message)
 
 
 };
+
+//how to get tokens for a specific promot useful for calculation of estimated costs
+const encodePrompt = (prompt: string) => {
+    //create an encoder for the model
+    const encoder = encoding_for_model("gpt-3.5-turbo");
+
+    //encode the prompt
+    const tokens = encoder.encode(prompt);
+    console.log(tokens);
+
+}
+
+encodePrompt("I need to start resistance training. is it good for me? Limit it in 10 words or less.")
 
 main();
 
