39ccfa22326173b6e6ee1a94032de6dbed4bcc36
Siddhi Sahu <himanisahu739@gmail.com>
2025-05-20 21:28:14 +0530

    pinecone index operations

--- chat-app/src/pinecone/index.ts
+++ chat-app/src/pinecone/index.ts
@@ -8,7 +8,12 @@ const pinecone = new Pinecone({
     apiKey: process.env.PINECONE_API_KEY
 });
 
-const main = async () => {
+type metadata = {
+    name: string;
+    age: number
+}
+
+const createIndex = async () => {
     await pinecone.createIndex({
         name: "siddhi-chat-app-index",
         dimension: 1536,
@@ -22,4 +27,66 @@ const main = async () => {
     })
 };
 
+const listIndex = async () => {
+    const indexes = await pinecone.listIndexes();
+    console.log(indexes);
+};
+
+const getIndex = async (indexName: string) => {
+    const index = pinecone.index<metadata>(indexName);
+    console.log(index);
+    return index;
+};
+
+
+// namespace: partition vector data from an index into multiple namespaces or groups. This will make the operations limited to a specific namespace.
+const createNameSpace = async () => {
+    const index = await getIndex("siddhi-chat-app-index");
+    const namespace = index.namespace("siddhi-chat-app-index-namespace");
+};
+
+// Generate random vectors
+const generateRandomVectors = async (length: number) => {
+
+    return Array.from({ length }, () => Math.random());
+};
+
+const upsertVectors = async () => {
+    const embeddings = await generateRandomVectors(1536);
+    const index = await getIndex("siddhi-chat-app-index");
+
+    await index.upsert([
+        {
+            id: "id-1",
+            values: embeddings,
+            metadata: {
+                name: "john",
+                age: 25
+            }
+        }
+    ]);
+    console.log("upsert success")
+};
+
+const queryVectors = async () => {
+    const index = await getIndex("siddhi-chat-app-index");
+
+    const result = await index.query({
+        id: "id-2",
+        //number of query results to returm
+        topK: 1,
+
+        includeMetadata: true
+    });
+
+    console.log(result);
+}
+
+const main = async () => {
+    // await createIndex
+    // await listIndex();
+    // getIndex('siddhi-chat-app-index')
+    // await upsertVectors();
+    await queryVectors();
+}
 main();
